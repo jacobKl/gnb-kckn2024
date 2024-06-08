788efd90c327ead086c596a47c934bd6b2f55343
Klimek Jakub <kqklimek@gmail.com>
2024-06-08 22:29:36 +0200

    All routes toggle route

--- resources/js/screens/AllRoutesScreen.jsx
+++ resources/js/screens/AllRoutesScreen.jsx
@@ -1,28 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer } from "react-leaflet";
 import SingleRoute from "../components/SingleRoute";
 
 function AllRoutesScreen() {
+    const [selectedRoute, setSelectedRoute] = useState(null);
     const { isLoading, error, data } = useQuery({
         queryKey: ["bigMapData"],
         queryFn: () =>
-            fetch("/api/get-described-routes").then((res) =>
-                res.json()
-            )
+            fetch("/api/get-described-routes").then((res) => res.json()),
     });
 
-    if (isLoading) return 'Loading...';
-    if (error) return 'Error';
+    if (isLoading) return "Loading...";
+    if (error) return "Error";
 
-    return <div>
-        <MapContainer center={[50.049683, 19.944544]} zoom={9} scrollWheelZoom={false}>
-            <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {data.map((route,j) => (<SingleRoute key={j} route={route}></SingleRoute>))}
-        </MapContainer>
-    </div>;
+    const handleClick = (route) => {
+        if (!selectedRoute) {
+            setSelectedRoute(route);
+            return;
+        }
+
+        if (route.id == selectedRoute.id) setSelectedRoute(null);
+        else setSelectedRoute(route);
+    };
+
+    return (
+        <div className="flex flex-row">
+            <div className="bg-white p-2 rounded shadow basis-1/4">
+                {data.map((route, j) => (
+                    <div
+                        className="bg-white my-4 rounded flex flex-col text-center items-center cursor-pointer"
+                        key={j}
+                        onClick={() => handleClick(route)}
+                    >
+                        <div className="text-xl font-bold">
+                            {route.route_short_name}
+                        </div>
+                        <div className="ps-2 text-sm text-gray-500">
+                            {route.route_long_name}
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="basis-3/4 h-full">
+                <MapContainer
+                    center={[50.049683, 19.944544]}
+                    zoom={9}
+                    scrollWheelZoom={false}
+                >
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    {selectedRoute ? (
+                        <SingleRoute route={selectedRoute} color={selectedRoute}></SingleRoute>
+                    ) : (
+                        data.map((route, j) => (
+                            <SingleRoute key={j} route={route}></SingleRoute>
+                        ))
+                    )}
+                </MapContainer>
+            </div>
+        </div>
+    );
 }
 
 export default AllRoutesScreen;
