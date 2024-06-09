6c554f7f563e5128c77e60526fc008b8b210f560
Michał Proć <michalproc4@gmail.com>
2024-06-09 08:57:33 +0200

    GTFSController.php <-> MainScreen.jsx

--- resources/js/screens/MainScreen.jsx
+++ resources/js/screens/MainScreen.jsx
@@ -41,6 +41,7 @@ function MainScreen() {
     const [tripSearched, setTripSearched] = useState(false);
     const [zoom, setZoom] = useState(12);
     const [center, setCenter] = useState({lat: 50.049683, lng: 19.944544});
+    const [showModal, setShowModal] = useState(false); // Added state variable for modal visibility
 
     const {isLoading, error, data, refetch} = useQuery({
         queryKey: ["routeData"],
@@ -71,6 +72,7 @@ function MainScreen() {
                 }
             }).then(res => res.json())
             console.log(emissionsResponse)
+            setShowModal(true); // Show the modal
         }
         setTripSearched(true);
     };
@@ -141,15 +143,6 @@ function MainScreen() {
                     </div>
                 ) : null
             }
-            {/* {tripSearched ? (
-                <a
-                    target="_blank"
-                    className="fixed bottom-20 z-30  bg-white right-4 rounded shadow p-3"
-                    href={`https://www.google.com/maps/dir/?api=1&origin=${location.lat},${location.lon}&destination=${secondStop.stop_lat},${secondStop.stop_lon}&travelmode=driving`}
-                >
-                    <FontAwesomeIcon icon={faMap} />
-                </a>
-            ) : null} */}
             <MapContainer
                 center={[50.049683, 19.944544]}
                 zoom={12}
@@ -187,7 +180,64 @@ function MainScreen() {
                 )) : null}
                 <MapSetter zoom={zoom} center={center}/>
             </MapContainer>
-            {/* {cmDisplayed ? <CarManager displayCM={changeCMDisplay} /> : null} */}
+            {showModal && (
+                <div id="default-modal" tabIndex="-1" aria-hidden="true"
+                     className="fixed inset-0 z-50 flex justify-center items-center w-full h-screen bg-black bg-opacity-50">
+                    <div className="relative p-4 w-full max-w-2xl max-h-full">
+                        <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                            <div
+                                className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                                <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                                    Terms of Service
+                                </h3>
+                                <button type="button"
+                                        className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                        data-modal-hide="default-modal"
+                                        onClick={() => setShowModal(false)}
+                                >
+                                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                         fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                              stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                    </svg>
+                                    <span className="sr-only">Close modal</span>
+                                </button>
+                            </div>
+                            <div className="p-4 md:p-5 space-y-4">
+                                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                    With less than a month to go before the European Union enacts new consumer privacy
+                                    laws
+                                    for its citizens, companies around the world are updating their terms of service
+                                    agreements to comply.
+                                </p>
+                                <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+                                    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect
+                                    on
+                                    May 25 and is meant to ensure a common set of data rights in the European Union. It
+                                    requires organizations to notify users as soon as possible of high-risk data
+                                    breaches
+                                    that could personally affect them.
+                                </p>
+                            </div>
+                            <div
+                                className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                                <button data-modal-hide="default-modal" type="button"
+                                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                        onClick={() => setShowModal(false)} // Hide the modal
+                                >
+                                    I accept
+                                </button>
+                                <button data-modal-hide="default-modal" type="button"
+                                        className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                        onClick={() => setShowModal(false)} // Hide the modal
+                                >
+                                    Decline
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
